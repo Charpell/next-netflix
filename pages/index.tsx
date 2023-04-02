2c529bfba1002ca88ccba771236b4eaab7713c58
Ebuka Umeh <charpellumeh@gmail.com>
2023-04-02 16:58:35 +0100

    movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import { getSession } from 'next-auth/react';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/ Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,10 +28,16 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   )
 }
